8240e6110bf5063adde50a8f428c043ca9217150
anh_pham <>
2023-10-27 09:39:23 +0700

    fkx

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,7 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=0905062524/nodeapp:1.0
+dockerpath=0905062524/pythonapp:1.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
